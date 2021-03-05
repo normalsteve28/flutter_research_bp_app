6bada2ce2b9dc704861b621bd290a9ca9072b733
redtag69 <66169052+redtag69@users.noreply.github.com>
2021-03-05 18:38:42 +0800

    Update configuration.py

--- pythonbackend/configuration.py
+++ pythonbackend/configuration.py
@@ -1,5 +1,5 @@
 import os 
-from access import username, password, database #probably unnecessary, edit your secret values according to your likes
+from access import username, password, localhost, database #probably unnecessary, edit your secret values according to your likes
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_ECHO = True
@@ -9,4 +9,4 @@ SQLALCHEMY_TRACK_MODIF = False #SQLALCHEMY_TRACK_MODIFICATIONS adds significant 
 '''  if however, you're using a different sql besides postgres, just modify the values
      for example: SQLALCHEMY_DATABASE_URI = "sqlite:///app.db if you are using SQlite
 '''
-SQLALCHEMY_DATABASE_URI= f"postgresql://{username}:{password}@localhost/{database}"
+SQLALCHEMY_DATABASE_URI= f"postgresql://{username}:{password}@{localhost}/{database}"
