b096458bba96c918d6b8ff430ef6d683428a5c6d
Sean Jacinto <seanjacinto@yahoo.com>
2021-03-06 23:34:12 +0800

    Added sys, dia, and heart rate to history boxes

---
+++ lib/tabs/activity_assets/bp_data.dart
@@ -0,0 +1,18 @@
+//Dart file that contains list of systolic, diastolic, heart rate
+// Is NOT final data source for BP and heart rate, only a temporary placeholder
+/* 
+The history boxes code make boxes starting from the first index.
+However, the history boxes are expected to show the newest data first.
+Lists usually add newer data at the last.
+Therefore, for these history boxes to show the most recent data, the newer data
+should be added at the FRONT instead.
+*/
+
+List<int> sys = [2];
+List<int> dia = [4];
+List<int> heartRate = [5];
+bool dataIsEqualNotEmpty = (sys.length > 0 &&
+    dia.length > 0 &&
+    heartRate.length > 0 &&
+    sys.length == dia.length &&
+    dia.length == heartRate.length);
